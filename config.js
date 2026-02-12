2b4a8d321ea828b094b9c647ac3a4f39681a1bc7
ΣΝΛ <78288792+4zx16@users.noreply.github.com>
2026-02-11 21:47:40 -0800

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ export const config = {
     UI: "https://piped.video/", // This loads the frontend for watch together, displays all videos
     sponsorBlock: "https://sponsor.ajay.app/", // Blocks all sponsors
     dearrow: "https://dearrow.ajay.app/", // Shows nonclick titles
-    API: "https://pipedapi.nosebs.ru/api/v1/" // Plays videos & fetches them
+    API: "https://pipedapi.nosebs.ru/api/v1/", // Plays videos & fetches them
+    Proxy: "https://cors-anywhere.herokuapp.com/"  // Bypass CORS errors and resolves them
 };
